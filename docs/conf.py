a04dbb76f33d62690988bff225a399f69497b910
Mark <MtG@live.ca>
2025-03-28 16:17:03 -0600

    feat: add performance benchmarking for ConcurrentBuffer and ConcurrentQueue

--- docs/conf.py
+++ docs/conf.py
@@ -4,25 +4,21 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'ThreadFactory'
-copyright = '2025, Mark Geleta'
 author = 'Mark Geleta'
+copyright = f'2025, {author}'
 release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -3,15 +3,20 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-ThreadFactory documentation
-===========================
+ThreadFactory
+============
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
-
+High-performance thread-safe data structures (No-GIL-friendly) for Python 3.13+.
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
