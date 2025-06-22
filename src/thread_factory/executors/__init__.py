2e10b8a144f885c6283dfe90aaedabf778ccf3b1
Mark <MtG@live.ca>
2025-06-22 11:47:43 -0600

    feat: introduce Dynapool and DynamicWorker classes for enhanced thread management

---
+++ src/thread_factory/executors/__init__.py
@@ -0,0 +1,8 @@
+from thread_factory.executors.dyna_pool import Dynapool
+from thread_factory.executors.worker.dynamic_worker import DynamicWorker
+
+
+__all__ = [
+    "Dynapool",
+    "DynamicWorker",
+    ]
